db596cb3cf4908865e8d6a32fca34a73f1f5206a
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-03-08 17:51:50 +0600

    Successfully Connected To Database

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { type Request, type Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// import walletRouter from "./app/modules/wallet/wallet.routes";
+// import adminRouter from "./app/modules/admin/admin.routes";
+// import { errorHandler } from "./app/middlewares/globalErrorHandler";
+// import authRouter from "./app/modules/auth/auth.routes";
+// import transactionRouter from "./app/modules/transaction/transaction.routes";
+
+
+ const app = express();
+
+// Middlewares
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173',
+//     // origin: 'https://digital-wallet-client-beta.vercel.app',
+//      credentials: true,
+//    })
+// );
+
+app.use(express.json());
+app.use(cookieParser());
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+// // Routes
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/wallet", walletRouter);
+// app.use("/api/v1/transactions", transactionRouter);
+// app.use("/api/v1/admin", adminRouter);
+
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Welcome to Headless Tech Server!');
+});
+// Error handling
+// app.use(errorHandler);
+
+export default app;
---
+++ src/app/config/env.ts
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+
+
+dotenv.config()
+interface EnvConfig {
+    PORT: string,
+    DB_URL: string,
+    // NODE_ENV: "development" | "production"
+    // BCRYPT_SALT_ROUND: string
+    // JWT_ACCESS_SECRET: string
+    // JWT_ACCESS_EXPIRES: string
+   
+    // ADMIN_PHONE: string
+    // ADMIN_PASSWORD: string
+   
+
+}
+
+const loadEnvVariables = (): EnvConfig => {
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL"];
+    // const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "ADMIN_PHONE", "ADMIN_PASSWORD",];
+
+    requiredEnvVariables.forEach(key => {
+        if (!process.env[key]) {
+            throw new Error(`Missing require environment variabl ${key}`)
+        }
+    })
+
+    return {
+        PORT: process.env.PORT as string,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        DB_URL: process.env.DB_URL!,
+        // NODE_ENV: process.env.NODE_ENV as "development" | "production",
+        // BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+        // JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        // JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+      
+        // ADMIN_PHONE: process.env.ADMIN_PHONE as string,
+        // ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
+    
+    }
+}
+
+export const envVars = loadEnvVariables()
