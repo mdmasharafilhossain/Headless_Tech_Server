5450908f13c10e6e7b0d5be6a0803ec097a07675
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-03-09 00:20:43 +0600

    Feedback Service Updated

---
+++ src/app/config/teamEmail.ts
@@ -0,0 +1,7 @@
+type TeamName = "frontend" | "backend" | "support";
+
+export const teamEmails: Record<TeamName, string> = {
+  frontend: "frontend@company.com",
+  backend: "backend@company.com",
+  support: "support@company.com",
+};
--- src/app/modules/feedback/feedback.service.ts
+++ src/app/modules/feedback/feedback.service.ts
@@ -1,25 +1,46 @@
+import { teamEmails } from "../../config/teamEmail";
 import { AiGenerateFeedback } from "../../llm/AiGenerateFeedback";
 import { AppError } from "../../utilis/AppError";
+import { sendEmail } from "../../utilis/sendEmail";
 import Feedback from "./feedback.model";
 import { FeedbackInput } from "./feedback.validation";
 
 export const createFeedback = async (feedbackPayload: FeedbackInput) => {
-    const { name, message } = feedbackPayload;
-    const aiAnalysisResult = await AiGenerateFeedback(message);
+  const { name, message } = feedbackPayload;
+  const aiAnalysisResult = await AiGenerateFeedback(message);
 
-    if (!aiAnalysisResult) {
+  if (!aiAnalysisResult){
 
-        throw AppError.internalError("Failed to analyze feedback");
+     throw AppError.internalError("Failed to analyze feedback");
 
-    }
+  }
+  const createdFeedback = await Feedback.create({
+    name,
+    message,
+    category: aiAnalysisResult.category,
+    priority: aiAnalysisResult.priority,
+    sentiment: aiAnalysisResult.sentiment,
+    team: aiAnalysisResult.team,
+  });
 
-    const createdFeedback = await Feedback.create({
-        name,
-        message,
-        category: aiAnalysisResult.category,
-        priority: aiAnalysisResult.priority,
-        sentiment: aiAnalysisResult.sentiment,
-        team: aiAnalysisResult.team,
-    });
-    return createdFeedback;
+  const teamEmail = teamEmails[aiAnalysisResult.team as keyof typeof teamEmails];
+
+  if (teamEmail){
+    await sendEmail(
+      teamEmail,
+      "New Feedback Received",
+      `
+        <h2>New Feedback Submitted</h2>
+        <p><b>Name:</b>  ${name}</p>
+        <p><b>Message:</b>   ${message}</p>
+        <p><b>Category:</b>   ${aiAnalysisResult.category}</p>
+        <p><b>Priority:</b>  ${aiAnalysisResult.priority}</p>
+        <p><b>Sentiment:</b>  ${aiAnalysisResult.sentiment}</p>
+        <p><b>Assigned Team:</b>  ${aiAnalysisResult.team}</p>
+      `
+    );
+
+  }
+
+  return createdFeedback;
 };
